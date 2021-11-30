5c8ae6178e990e686290957fd98bdca35259a8dd
raingggg <raingggg@nomail.com>
2021-11-30 09:39:30 +0800

    web0.1.3

--- web_launch.sh
+++ web_launch.sh
@@ -1,4 +1,2 @@
-if [[ ${mode} == 'fullnode' ]]; then
-  echo 'coctohug-flora web_launch working'
-  npm start --prefix /coctohug/web
-fi
+echo 'coctohug-flora web_launch working'
+npm start --prefix /coctohug/web
